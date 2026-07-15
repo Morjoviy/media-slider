a94a4039fa7232c1073f517401f603d27a8e11a0
Morjoviy <myredbeardremote@gmail.com>
2026-07-16 01:25:38 +0500

    Revert tablet breakpoint changes

--- media-slider.js
+++ media-slider.js
@@ -1,7 +1,7 @@
 (() => {
   const CONFIG = {
     bunnyStreamHostname: "vz-f88e5e4c-151.b-cdn.net",
-    mobileBreakpoint: 1279,
+    mobileBreakpoint: 991,
     desktopOffsetRatio: 1 / 6,
     mobileOffset: 0,
     slideGap: 10,
